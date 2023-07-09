856eb50053ff0381190d1cab03d9077c4fd6471f
parth782 <parth11gupta@gmail.com>
2023-07-09 19:10:47 +0530

    some files removed

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -33,14 +33,13 @@ class Homepage extends React.Component {
           >
             <div id="description" className={`w-3/4 mx-auto`}>
               <h3 className="font-poppins font-semibold xs:text-[40.45px] text-[30.45px] xs:leading-[56.58px] leading-[38.58px] text-gradient uppercase ml-3">
-                Welcome to the Krishi Market!
+                Welcome to the Crafter Bazaar!
               </h3>
               <h5 className="font-poppins font-semibold xs:text-[18.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-gradient sentencase ml-3">
-                A place where local farmers can create their own inventory,
-                update it, and sell farm-fresh products to everyone who is
-                interested in fresh local produce. Shoppers can search for the
-                products they are interested in and find the farms that sell
-                those products.
+                A place where handicrafts can create their own inventory,
+                update it, and sell  products to everyone who is
+                interested in unique products. Shoppers can search for the
+                products they are interested in and find the appropriate seller who sell those products.
               </h5>
             </div>
             <ShoppersSearch />
