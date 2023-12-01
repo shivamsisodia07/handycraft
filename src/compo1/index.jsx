f3ca33628a949695a5577779040764df1e89e858
parth782 <parth11gupta@gmail.com>
2023-12-02 04:57:15 +0530

    extra folder removed

--- src/compo1/index.jsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "./navbar";
-import Billing from "./billing";
-import Business from "./business";
-import Clients from "./clients";
-import Stats from "./stats";
-import Footer from "./footer";
-import Testimonials from "./testimonials";
-
-export { Navbar, Billing, Business, Clients, Stats, Footer, Testimonials };
--- src/compo1/services/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    API_ENDPOINT: "https://festive-planet-353313.el.r.appspot.com/api",
-    //API_ENDPOINT: "http://localhost:8000/api",
-    TOKEN_KEY: 'farmers-bazaar'
-  }
